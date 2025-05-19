ea1ca366ea1e2314edc9d96b0fbcb7c2c41bbd0f
hmdfurniture <bohmdfurniture@gmail.com>
2025-05-19 14:40:05 +0100

    Update dataLoader.js

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -1,1 +1,21 @@
+// dataLoader.js
+window.supplierData = [];
+window.conversionFactorsInternational = {};
+window.conversionFactorsNational = {};
 
+window.loadSupplierData = function(callback) {
+    Promise.all([
+        fetch('./Tables/xbslog_international.json').then(response => response.json()),
+        fetch('./Tables/xbslog_nacional.json').then(response => response.json())
+    ])
+    .then(([internationalData, nacionalData]) => {
+        window.supplierData = [
+            ...internationalData.destinations,
+            ...nacionalData.destinations
+        ];
+        window.conversionFactorsInternational = internationalData.conversion || {};
+        window.conversionFactorsNational = nacionalData.conversion || {};
+        if (typeof callback === "function") callback();
+    })
+    .catch((error) => console.error('Error loading supplier data:', error));
+};
